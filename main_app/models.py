3b1c756ce96d2b5a5d327c6e7f8c492d77c0043d
Yi Li <youremail@domain.com>
2022-05-11 23:06:29 -0700

    part5

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,24 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.urls import reverse
+
+
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+  # Toys have a M:M related manager named
+  # cat_set
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
 
 # Create your models here.
 class Finch(models.Model):
@@ -13,10 +29,29 @@ class Finch(models.Model):
   # TextField will create a <textarea>
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return f'{self.name} ({self.id})'
     
-
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
+
+  
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,14 @@
-from django.shortcuts import render
-from .models import Finch
+from django.shortcuts import render,redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 from django.http import HttpResponse
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-] 
-
 def home(request):
-    return HttpResponse('<h1>home page</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -29,4 +19,60 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finches_detail})
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id_in=id_list)
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+     'finch': finch, 'feeding_form': feeding_form
+     })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+  success_url =  '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
+
+  def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+      # don't save the form to the db until it
+      new_feeding = form.save(commit=False)
+      new_feeding.finch_id = finch_id
+      new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy 
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
