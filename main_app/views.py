ca70c08f03c9722f734d4cedca605e7b222d94a8
Yi Li <youremail@domain.com>
2022-05-12 18:29:16 -0700

    finish lab 5

--- main_app/views.py
+++ main_app/views.py
@@ -20,10 +20,10 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   id_list = finch.toys.all().values_list('id')
-  toys_finch_doesnt_have = Toy.objects.exclude(id_in=id_list)
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-     'finch': finch, 'feeding_form': feeding_form
+     'finch': finch, 'toys': toys_finch_doesnt_have, 'feeding_form': feeding_form
      })
 
 class FinchCreate(CreateView):
